25405e47a34eea136ed5928afe3399e2ce23c12c
eramirezcarlos <tequendama.developer@gmail.com>
2023-09-02 17:59:08 -0400

    Build an application

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,6 @@ function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
-      <ul>
-        <li><Link href="/portfolio">Portfolio</Link></li>
-        <li><Link href="/clients">Clients </Link></li>
-      </ul>
     </div>
   );
 }
