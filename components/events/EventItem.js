9ec99700449b7f31f09cd5fc5004e3f6473c3ce7
eramirezcarlos <eramirezcarlos@gmail.com>
2023-09-09 08:38:01 -0400

    Additions of icons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,46 +1,52 @@
 import Link from "next/link";
-import classes from './EventItem.module.css';
+import classes from "./EventItem.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   //console.log( "desde item ", props.item );
   const { title, image, date, location, id } = props;
 
-  const humanReadableDate = new Date( date  ).toLocaleDateString('en-US',{
-    day:'numeric',
-    month: 'long',
-    year: 'numeric'
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
 
-  const formattedAddress = location.replace(', ','\n');
+  const formattedAddress = location.replace(", ", "\n");
 
   const exploreLink = `/events/${id}`;
 
   return (
     <li key={id} className={classes.item}>
-
-      <img src={'/'+image} alt={title} />
+      <img src={"/" + image} alt={title} />
 
       {/* div for the info */}
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
-        
+
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
 
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
-
-        </div> 
-        <div className={classes.actions}>
-
-          <Button link={exploreLink}>Explore Event</Button>
         </div>
-
-      </div>  
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
     </li>
   );
 }
